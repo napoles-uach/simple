91203d40bcc927f444940c77c5883ed3d3bbdbc1
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-02-15 13:31:25 -0700

    new

--- simple.py
+++ simple.py
@@ -3,15 +3,19 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from spring import spring
-st.title('some title')
+st.title('Ley de Hooke')
+k=st.number_input('Constante de fuerza K:',5)
 #st.button('click')
 #y=st.number_input('give pos y')
-y = st.slider('give pos y',4,10,5)
-point_a = (0, 0)
-point_b = (0, -y)
+masa = st.slider('masa',0.1,1.0,0.0)
+y=-(masa*9.8/k)*100
 pe=-4
+st.write(round(y,4),round(y+pe,4))
+point_a = (0, 0)
+point_b = (0, y+pe)
+
 fig, ax = plt.subplots(figsize=(5, 5))
-patch = patches.Circle((0, -y), radius=1, alpha =1)
+patch = patches.Circle((0, y+pe), radius=1, alpha =1)
 ax.add_patch(patch)
 
 ax.plot(*spring(point_a, point_b, 12, 1.2), c="blue")
@@ -25,4 +29,3 @@ ax.annotate('punto de equilibrio', xy=(1, pe), xytext=(3, pe+0.2),
 
 #plt.show()
 st.pyplot(fig)
-
