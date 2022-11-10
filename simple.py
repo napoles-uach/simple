8a2165b700a8f814df6e554425ab9ce59ce8fad0
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-11-10 13:33:57 -0600

    Update simple.py

--- simple.py
+++ simple.py
@@ -21,7 +21,7 @@ ax.add_patch(patch)
 ax.plot(*spring(point_a, point_b, 12, 1.2), c="blue")
 ax.set_aspect("equal", "box")
 ax.axes.set_xlim(-10,10)
-ax.axes.set_ylim(-10,0)
+ax.axes.set_ylim(-20,0)
 ax.annotate('punto de equilibrio', xy=(1, pe), xytext=(3, pe+0.2),
             arrowprops=dict(facecolor='black', shrink=0.05),
             )
